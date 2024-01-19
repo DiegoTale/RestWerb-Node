594c442a2df8464e0c9a7197339fbefdd703a2bc
Diego Tale <dperezt1@miumg.edu.gt>
2024-01-18 19:28:59 -0600

    error en datasource.impl.ts

---
+++ src/domain/index.ts
@@ -0,0 +1,5 @@
+export * from "./datasource/todo.datasource";
+export * from "./dtos";
+export * from "./entities/todo.entity";
+// export * from "./infrastructure/datasource/todo.datasource.impl";
+export * from "./repositories/todo.respository";
